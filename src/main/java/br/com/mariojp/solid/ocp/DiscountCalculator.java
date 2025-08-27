7575ee1494c63ff630e51e6c50b8bc27cdf42c63
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-08-27 12:25:54 -0300

    Refactor DiscountCalculator to use policy pattern

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,29 @@
 package br.com.mariojp.solid.ocp;
 
+import br.com.mariojp.solid.ocp.policies.PartnerPolicy;
+import br.com.mariojp.solid.ocp.policies.PremiumPolicy;
+import br.com.mariojp.solid.ocp.policies.RegularPolicy;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DiscountCalculator {
-    public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
-        }
+    private final Map<CustomerType, DiscountPolicy> policies;
+
+    // Construtor padrão para compatibilidade com Main e Test
+    public DiscountCalculator() {
+        this.policies = new HashMap<>();
+        this.policies.put(CustomerType.REGULAR, new RegularPolicy());
+        this.policies.put(CustomerType.PREMIUM, new PremiumPolicy());
+        this.policies.put(CustomerType.PARTNER, new PartnerPolicy());
+    }
+
+    public DiscountCalculator(Map<CustomerType, DiscountPolicy> policies) {
+        this.policies = policies;
+    }
+
+    public double apply(double amount, CustomerType type) {
+        DiscountPolicy policy = policies.get(type);
+        return (policy != null) ? policy.apply(amount) : amount;
     }
 }
